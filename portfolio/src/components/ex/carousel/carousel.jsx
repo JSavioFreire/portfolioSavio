3b33dacbe7ad53a956468328e7bc7e9fda99447f
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-12-03 13:30:04 -0300

    carousel

--- portfolio/src/components/ex/carousel/carousel.jsx
+++ portfolio/src/components/ex/carousel/carousel.jsx
@@ -6,6 +6,9 @@ import "swiper/css/pagination";
 
 import "./styles.css";
 
+import pokedexImg from '../../../imageCarousel/pokedex.png'
+import movieImg from '../../../imageCarousel/movie.png'
+
 import { Pagination } from "swiper";
 
 export default function CarouselPro() {
@@ -20,15 +23,9 @@ export default function CarouselPro() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide><img src={pokedexImg} /></SwiperSlide>
+        <SwiperSlide><img src={movieImg} /></SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
